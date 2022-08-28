08d08fe2270cdc514260ffe500aa816c74067877
Viktoriia Malysh <pylypenko.viktoriia@gmail.com>
2022-08-28 19:25:04 +0200

    add 1 and 2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function addGalleryItems (galleryItems) {
+   const pictures = galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"/>
+             </a>
+        </div>`
+    }).join('');
+
+    gallery.insertAdjacentHTML('afterbegin', pictures);
+};
+
+addGalleryItems(galleryItems);
+
+gallery.addEventListener('click', onClickModal);
+
+function onClickModal(e) {
+    e.preventDefault();
+    if(e.target.nodeName !== "IMG") {
+        return;
+    }
+
+   const instance = basicLightbox.create(`<img src="${e.target.dataset.source}">`,
+    { onShow: () => {window.addEventListener('keydown', onEsc) }},)
+
+    console.log(e.target);
+
+    instance.show();
+
+    function onEsc(e) {
+        if (e.key === 'Escape') {
+            instance.close();
+            window.removeEventListener('keydown', onEsc);
+        }
+    }
+
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function addGalleryItems (galleryItems) {
+   const pictures = galleryItems.reduce((acc, {preview, original, description}) => {
+        return acc + `
+        <li class="gallery__item">
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+
+
+    }, '')
+
+    gallery.insertAdjacentHTML('afterbegin', pictures);
+};
+
+addGalleryItems(galleryItems);
+
+new SimpleLightbox('.gallery a', { fadeSpeed: 500, captionDelay: 250, captionsData: "alt", scrollZoom: true, });
